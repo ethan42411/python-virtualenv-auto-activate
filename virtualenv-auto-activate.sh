869fb7a38b1de6e09b922ff1d4d68136ed468449
Ethan Chen <ethanchen@iii.org.tw>
2021-04-16 10:01:22 +0800

    fix: exit virtualenv directory deactivate failed

--- virtualenv-auto-activate.sh
+++ virtualenv-auto-activate.sh
@@ -3,23 +3,25 @@
 # The virtualenv will be activated automatically when you enter the directory.
 
 _virtualenv_auto_activate() {
-      if [ -e "bin/activate" ]; then
-          # Check to see if already activated to avoid redundant activating
-          if [ "$VIRTUAL_ENV" = "" ]; then
-              _VENV_NAME=$(basename `pwd`)
-              echo Activating virtualenv \"$_VENV_NAME\"...
-              VIRTUAL_ENV_DISABLE_PROMPT=1
-              source bin/activate
-              _OLD_VIRTUAL_PS1="$PS1"
-              PS1="($_VENV_NAME)$PS1"
-              export PS1
-           fi
-     else
-       if [ "$VIRTUAL_ENV" != "" ]; then
-         echo deactivating VirtualEnv
-         deactivate
-       fi
-     fi
-   }
+  if [ -e "bin/activate" ]; then
+    # Check to see if already activated to avoid redundant activating
+    if [ "$VIRTUAL_ENV" = "" ]; then
+      _VENV_NAME=$(basename `pwd`)
+      echo Activating virtualenv \"$_VENV_NAME\"...
+      VIRTUAL_ENV_DISABLE_PROMPT=1
+      source bin/activate
+      _OLD_VIRTUAL_PS1="$PS1"
+      PS1="($_VENV_NAME)$PS1"
+      export PS1
+    fi
+  else
+    if [ "$VIRTUAL_ENV" != "" ]; then
+      if [[ "$PWD"/ != "$VIRTUAL_ENV"/* ]] ; then
+        echo deactivating virtualenv
+        deactivate
+      fi
+    fi
+  fi
+}
 
 export PROMPT_COMMAND=_virtualenv_auto_activate
