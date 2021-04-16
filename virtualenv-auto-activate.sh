711a81037b55f32515f3ee04f35106703604af2d
Ethan Chen <ethanchen@iii.org.tw>
2021-04-16 09:51:59 +0800

    feat: add auto deactivate

--- virtualenv-auto-activate.sh
+++ virtualenv-auto-activate.sh
@@ -1,19 +1,25 @@
 #!/bin/bash
 # python-virtualenv-auto-activate
 # The virtualenv will be activated automatically when you enter the directory.
+
 _virtualenv_auto_activate() {
-    if [ -e "bin/activate" ]; then
-        # Check to see if already activated to avoid redundant activating
-        if [ "$VIRTUAL_ENV" != "$(pwd -P)" ]; then
-            _VENV_NAME=$(basename `pwd`)
-            echo Activating virtualenv \"$_VENV_NAME\"...
-            VIRTUAL_ENV_DISABLE_PROMPT=1
-            source bin/activate
-            _OLD_VIRTUAL_PS1="$PS1"
-            PS1="($_VENV_NAME)$PS1"
-            export PS1
-        fi
-    fi
-}
+      if [ -e "bin/activate" ]; then
+          # Check to see if already activated to avoid redundant activating
+          if [ "$VIRTUAL_ENV" = "" ]; then
+              _VENV_NAME=$(basename `pwd`)
+              echo Activating virtualenv \"$_VENV_NAME\"...
+              VIRTUAL_ENV_DISABLE_PROMPT=1
+              source bin/activate
+              _OLD_VIRTUAL_PS1="$PS1"
+              PS1="($_VENV_NAME)$PS1"
+              export PS1
+           fi
+     else
+       if [ "$VIRTUAL_ENV" != "" ]; then
+         echo deactivating VirtualEnv
+         deactivate
+       fi
+     fi
+   }
 
 export PROMPT_COMMAND=_virtualenv_auto_activate
